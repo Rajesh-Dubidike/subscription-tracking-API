29599c18a97fe86a15809d76243cd57a918ed1ac
rajesh-raj-raj <23211a0576@bvrit.ac.in>
2025-08-28 22:05:12 +0530

    error handler & authorization

--- subscription tracking/database/mongodb.js
+++ subscription tracking/database/mongodb.js
@@ -9,7 +9,7 @@ const connectToDatabase = async () => {
     try {
         await mongoose.connect(DB_URI);
         
-            console.log(`Connected to database in ${NODE_ENV} mode`);
+            console.log(`Connected to database i ${NODE_ENV} mode`);
         
     } catch (error) {
         console.error('Error connecting to database:', error);
--- subscription tracking/routes/auth.routes.js
+++ subscription tracking/routes/auth.routes.js
@@ -1,12 +1,12 @@
-
+import { signup ,signIn} from '../controllers/auth.controller.js';
 
 import { Router } from 'express';
 
 const authRouter= Router();
 
-authRouter.post('/sign-up', (req, res) => res.send({ title: 'Sign up' }));
+authRouter.post('/sign-up', signup);
 
-authRouter.post('/sign-in', (req, res) => res.send({ title: 'Sign in' }));
+authRouter.post('/sign-in', signIn); ;
 authRouter.post('/sign-out', (req, res) => res.send({ title: 'Sign out' }));
 
 export default authRouter;
